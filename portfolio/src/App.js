084d650872c36b9a241f7dacb8b57e1efe2a509b
xyiorgyx <112989524+xyiorgyx@users.noreply.github.com>
2023-04-01 16:43:10 -0400

    Projects
    Added data for projects that I've completed and created the page. Needs debugging.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,1 +1,13 @@
 import React from "react";
+// import Nav from "./components/Nav";
+import Projectcards from "./components/projectpage"
+import projects from "./projects";
+
+export default function App() {
+  console.log(projects)
+    return (
+      <div>
+        <Projectcards projects = {projects} />
+      </div>
+    );
+  }
