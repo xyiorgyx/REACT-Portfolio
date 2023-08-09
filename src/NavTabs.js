228dd2eef03b75b1bc2834ad76e50990220ba62d
xyiorgyx <112989524+xyiorgyx@users.noreply.github.com>
2023-08-09 18:56:19 -0400

    Additional designing
    Added photo of myself as well as some decorative elements.
    Nav bar now shows up but functionality needs to be completed.

--- src/NavTabs.js
+++ src/NavTabs.js
@@ -2,19 +2,19 @@ import React from 'react';
 
 const Styles = {
   Navheader: {
-  top:'4%',
-  left: '20%',
-  height: '10%',
-  width: '20%',
-  position: 'absolute',
+    top: '3%',
+    left: '20%',
+    position: 'absolute',
+    width:'40%',
+    gap: '10px',
   },
 
   NavList: {
-   display: 'flex',
-   padding: '10px',
-   gap: '48px',
-   justifyContent: 'center',
-   alignItems:'center'
+    display: 'flex',
+    padding: '10px',
+    gap: '12%',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
 
   NavButtons: {
@@ -24,56 +24,65 @@ const Styles = {
     fontStyle: 'normal',
     fontWeight: '400',
     lineHeight: 'normal',
-  }
+    textDecoration: 'none', // Remove underline from normal state
+    transition: 'color 0.3s', // Smooth color transition
+   
+  },
+
+
+
 }
+
+
 export default function NavTabs({ currentPage, handlePageChange }) {
   return (
 
-  <div style = {Styles.Navheader}>
-    <ul style={Styles.NavList} className="nav nav-tabs">
-      <li style={Styles.NavButtons} className="nav-item">
-        <a
-          href="#home"
-          onClick={() => handlePageChange('Home')}
-          // This is a conditional (ternary) operator that checks to see if the current page is "Home"
-          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
-        >
-          Home
-        </a>
-      </li>
-      <li style={Styles.NavButtons} className="nav-item">
-        <a
-          href="#about"
-          onClick={() => handlePageChange('About')}
-          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-        >
-          About
-        </a>
-      </li>
-      <li style={Styles.NavButtons} className="nav-item">
-        <a
-          href="#projectpage"
-          onClick={() => handlePageChange('ProjectPage')}
-          // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'Blog' ? 'nav-link active' : 'nav-link'}
-        >
-          Blog
-        </a>
-      </li>
-      <li style={Styles.NavButtons} className="nav-item">
-        <a
-          href="#contact"
-          onClick={() => handlePageChange('Contact')}
-          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-        >
-          Contact
-        </a>
-      </li>
-    </ul>
-</div>
+    <div style={Styles.Navheader}>
+      <ul style={Styles.NavList} className="nav nav-tabs">
+        <li style={Styles.NavButtons}
+          className="navItem">
+          <a
+            href="#home"
+            onClick={() => handlePageChange('Home')}
+            // This is a conditional (ternary) operator that checks to see if the current page is "Home"
+            // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+            className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+          >
+            Home
+          </a>
+        </li>
+        <li style={Styles.NavButtons} className="navItem">
+          <a
+            href="#about"
+            onClick={() => handlePageChange('About')}
+            // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+            className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+          >
+            About
+          </a>
+        </li>
+        <li style={Styles.NavButtons} className="navItem">
+          <a
+            href="#projectpage"
+            onClick={() => handlePageChange('ProjectPage')}
+            // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+            className={currentPage === 'Blog' ? 'nav-link active' : 'nav-link'}
+          >
+            Blog
+          </a>
+        </li>
+        <li style={Styles.NavButtons} className="navItem">
+          <a
+            href="#contact"
+            onClick={() => handlePageChange('Contact')}
+            // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+            className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+          >
+            Contact
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 }
 
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,24 +1,100 @@
 import React from 'react';
 import BackgroundImage from '../images/background.png'
 import NavTabs from '../../NavTabs';
+import Selfie from '../images/me.png'
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
 
 const Styles = {
   background: {
-  backgroundImage: `url(${BackgroundImage})`,
-  backgroundSize: 'contain',
-  height: '100vh',
-  width: '100vw',
-  position: 'fixed'
-  }
+    backgroundImage: `url(${BackgroundImage})`,
+    backgroundSize: 'contain',
+    height: '100vh',
+    width: '100vw',
+    position: 'fixed'
+  },
+  brightCornerLight: {
+    width: '404px',
+    height: '387px',
+    flexShrink: '0',
+    borderRadius: '404px',
+    background: '#5665EF',
+    filter: 'blur(100px)',
+    position: 'absolute',
+    left: '-7%',
+    top: '-8%'
+  },
+  brightTopLight: {
+    width: '448px',
+    height: '425px',
+    top:'-33%',
+    right:'30%',
+    flexShrink: '0',
+    borderRadius: '448px',
+    background: 'var(--white, #FFFDF3)',
+    filter: 'blur(100px)',
+    position: 'absolute'
+  },
+  selfiePicture: {
+    position: 'relative',
+    left: '57%',
+    width: '696px',
+    height: '905px',
+    flexShrink: '0',
+  },
+  Name: {
+    /* Desktop/H1 */
+    fontFamily: 'MuseoModerno',
+    fontSize: '120px',
+    fontStyle: 'normal',
+    fontWeight: '700',
+    lineHeight: '125%',
+    display: 'relative',
+    width: '100%',
+    color: 'var(--white, #FFFDF3)',
+  },
+  Welcome: {
+    display: 'flex',
+  width: '1920px',
+  padding: '30px 10px',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: '10px',
+  },
 };
 
 
 export default function Home() {
   return (
-    <div style={Styles.background}>
-      <NavTabs></NavTabs>
+    <div
+      className='background'
+      style={Styles.background}>
+
+      <NavTabs>
+      </NavTabs>
+
+      <div className='cornerLight'
+        style={Styles.brightCornerLight}>
+      </div>
+
+      <div className='cornerLight'
+        style={Styles.brightTopLight}>
+      </div>
+
+      <img className='selfie'
+        src={Selfie}
+        style={Styles.selfiePicture} />
+
+      <h1 className='name'
+       styles= {Styles.Name}>
+      Yiorgos Bosnakis
+      </h1>
+
+      <h2 
+      style={Styles.mainHeader}>
+      Welcome, Im excited to have you here
+      </h2>
+
     </div>
 
   );
