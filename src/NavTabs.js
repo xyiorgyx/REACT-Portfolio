ae88e50b28e43814464144dcf6916cb481ab023d
xyiorgyx <112989524+xyiorgyx@users.noreply.github.com>
2023-11-22 09:24:18 -0500

    Nav Bar
    Nav bar now has an underline for hovering effect and changes color depending on which page is currently active.

--- src/NavTabs.js
+++ src/NavTabs.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import './components/styles/styleNavTabs.css'
+import './components/styles/styleNavTabs.css';
 
 export default function NavTabs({ currentPage, handlePageChange }) {
   return (
     <div>
-      <div></div>
       <ul className="nav-nav-tabs">
-        <li
-          className="navItem">
+        <li className="navItem"
+          >
           <a
             href="#home"
             onClick={() => handlePageChange('Home')}
-            // This is a conditional (ternary) operator that checks to see if the current page is "Home"
-            // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-            className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+            className={`nav-link ${currentPage === 'Home' ? 'active' : ''}`}
           >
             Home
           </a>
@@ -22,8 +19,7 @@ export default function NavTabs({ currentPage, handlePageChange }) {
           <a
             href="#resume"
             onClick={() => handlePageChange('Resume')}
-            // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-            className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+            className={`nav-link ${currentPage === 'Resume' ? 'active' : ''}`}
           >
             Resume
           </a>
@@ -32,8 +28,7 @@ export default function NavTabs({ currentPage, handlePageChange }) {
           <a
             href="#projectpage"
             onClick={() => handlePageChange('ProjectPage')}
-            // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-            className={currentPage === 'ProjectPage' ? 'nav-link active' : 'nav-link'}
+            className={`nav-link ${currentPage === 'ProjectPage' ? 'active' : ''}`}
           >
             Projects
           </a>
@@ -42,14 +37,13 @@ export default function NavTabs({ currentPage, handlePageChange }) {
           <a
             href="#contact"
             onClick={() => handlePageChange('Contact')}
-            // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-            className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+            className={`nav-link ${currentPage === 'Contact' ? 'active' : ''}`}
           >
             Contact
           </a>
         </li>
       </ul>
-      </div>
+    </div>
   );
 }
 
